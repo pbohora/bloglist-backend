4f983aa601c6e2e9678091132f1035134466ace5
pbohora <pradeepbohora26@gmail.com>
2020-02-27 14:56:55 +0200

    add new build

---
+++ build/precache-manifest.d62462c7e7cf73a27c8305462bc42f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4993b5eb2c84e3bd72655531824e39dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7843df51dc7858672734",
+    "url": "/static/css/main.43a78b94.chunk.css"
+  },
+  {
+    "revision": "10ae87b80ef96c4938d0",
+    "url": "/static/js/2.485ba3ff.chunk.js"
+  },
+  {
+    "revision": "7843df51dc7858672734",
+    "url": "/static/js/main.6eb7fd52.chunk.js"
+  },
+  {
+    "revision": "ede905dc564cd93ff4d1",
+    "url": "/static/js/runtime-main.4aa1f46a.js"
+  },
+  {
+    "revision": "76f3637448e8cf6a87a7ebf22e916a01",
+    "url": "/static/media/addblog.76f36374.png"
+  }
+]);
